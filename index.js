78e8da57594602955541f677f512ae6bc509029e
Christian Carballo <carballo.chr@gmail.com>
2021-11-01 22:42:18 -0300

    Ya no se repiten

--- index.js
+++ index.js
@@ -1,119 +1,63 @@
-const img = new Image(); 
-
- 
-
-
-const imag1 = img.src = 'https://sites.google.com/site/informaticaprueba1emmanuel/_/rsrc/1395877037694/dohko-de-libra/hqdefault.jpg'
-
-const imag2 = img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Mapa_CR.svg/1200px-Mapa_CR.svg.png"
-
-const imag3 = img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Costa_Rica_on_the_globe_%28Americas_centered%29.svg/1024px-Costa_Rica_on_the_globe_%28Americas_centered%29.svg.png"
-
-const imag4 = img.src = "Fotos/IMG_20210319_164635.jpg"
-
-const imag5 = img.src = "https://www.tuguiasana.org/wp-content/uploads/2018/10/Fuente_de_Parque_Chacabuco-650x500.jpg" 
-
-const imag6 = img.src = "https://contentv2.tap-commerce.com/cover/large/9789500396332_1.jpg?id_com=1113"
-
-const imag7 = img.src = "https://www.penguinlibros.com/uy/239669-large_default/danza-de-dragones-5.jpg"
-
-const imag8 = img.src = "https://www.educaciontrespuntocero.com/wp-content/uploads/2017/11/peliculas_prehistoria_destacada.jpg" 
-
-const imag9 = img.src = "https://upload.wikimedia.org/wikipedia/commons/a/ab/Mate_pastor_ajedrez.gif"
-
-const imag10 = img.src = "https://static.wikia.nocookie.net/doblaje/images/9/9a/Elpadrino.jpg/revision/latest?cb=20170311061852&path-prefix=es"
-
-const imag11 = img.src = "https://pics.filmaffinity.com/American_History_X-201185607-large.jpg"
-
-const imag12A = img.src = "https://i0.wp.com/www.scienceofnoise.net/wp-content/uploads/2020/09/rust-in-peace-5cd5b40e937e4.jpg"
-
-const imag12B = img.src = "https://heraldodemexico.com.mx/u/fotografias/m/2021/3/1/f608x342-328853_358576_0.jpg"
-
-const imag12C = img.src = "https://img.discogs.com/zJZ1m-xOfqGS2Rmo7svcBHw1gsU=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-169325-1316298017.jpeg.jpg"
-
-const imagFinal = img.src = "https://www.storyplot.com/wp-content/uploads/2015/06/ThatsALLjpg.jpg"
-
-
-let bandasFavoritas = [
-
-    { 
-        texto: 'Megadeth',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag12A
-    },
-    { 
-        texto: 'Pink Floyd',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag12B
-    },
-    { 
-        texto: 'Outkast',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag12C
-    },
-
-]
-
 
 let datos =  [
     { 
-    texto:'Tengo 25 años y cumplo el 04 de Octubre.',
-    repeticiones: 0, //variable vacía para almacenar el numero de intentos
-    imagen: imag1,
-    tipoImagen : "horizontal"
+        texto:'Tengo 25 años y cumplo el 04 de Octubre.',
+        repeticiones: 0, //variable vacía para almacenar el numero de intentos
+        imagen: new Image().src = 'https://sites.google.com/site/informaticaprueba1emmanuel/_/rsrc/1395877037694/dohko-de-libra/hqdefault.jpg',
+        tipoImagen : "horizontal"
     },
     { 
-        texto:'Nací en Costa Rica',
+        texto:'Soy de Costa Rica',
         repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag2,
+        imagen: new Image().src ="https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Mapa_CR.svg/1200px-Mapa_CR.svg.png",
         tipoImagen : "cuadrada"
     },
     { 
-        texto: 'Vivo solo, en Buenos Aires, en el barrio Parque Chacabuco.',
+        texto: 'Vivo en Parque Chacabuco.',
         repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag5,
+        imagen: new Image().src ="https://www.tuguiasana.org/wp-content/uploads/2018/10/Fuente_de_Parque_Chacabuco-650x500.jpg" ,
         tipoImagen : "cuadrada"
     },
     { 
         texto: ' Mi libro favorito es "Hombres de Maíz" de Miguel Angel de Asturias',
         repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag6,
+        imagen: new Image().src ="https://contentv2.tap-commerce.com/cover/large/9789500396332_1.jpg?id_com=1113",
         tipoImagen : "vertical"
     },
     { 
-        texto: 'Actualmente estoy leyendo el quinto libro de la Saga de "Canción de Hielo y Fuego" de George R.R. Martin.',
+        texto: 'Actualmente estoy leyendo la Saga de "Canción de Hielo y Fuego" de George R.R. Martin.',
         repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag7,
+        imagen: new Image().src = "https://www.penguinlibros.com/uy/239669-large_default/danza-de-dragones-5.jpg",
         tipoImagen : "vertical"
     },
     { 
         texto: 'Sé tocar un poco la guitarra y tengo una banda llamada "Sidra"',
         repeticiones: 0, //variable vacía para almacenar el numero de intentos
-       imagen: imag8,
+        imagen: new Image().src ="https://www.educaciontrespuntocero.com/wp-content/uploads/2017/11/peliculas_prehistoria_destacada.jpg" ,
        tipoImagen : "horizontal"
     },
     { 
         texto: 'Soy horrible jugando al ajedrez pero juego seguido.',
         repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag9,
+        imagen: new Image().src ="https://upload.wikimedia.org/wikipedia/commons/a/ab/Mate_pastor_ajedrez.gif",
         tipoImagen : "cuadrada"
     },
     { 
         texto: 'Mi película favorita es "El Padrino"',
         repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag10,
+        imagen: new Image().src ="https://static.wikia.nocookie.net/doblaje/images/9/9a/Elpadrino.jpg/revision/latest?cb=20170311061852&path-prefix=es",
         tipoImagen : "vertical"
     },
     { 
         texto: 'Mi segunda película favorita es "American History X"',
         repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag11,
+        imagen: new Image().src ="https://pics.filmaffinity.com/American_History_X-201185607-large.jpg",
         tipoImagen : "vertical"
     },
     { 
         texto: 'Mi banda favoita es: ',
         repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: imag12A,
+        imagen: new Image().src = "https://i0.wp.com/www.scienceofnoise.net/wp-content/uploads/2020/09/rust-in-peace-5cd5b40e937e4.jpg",
         tipoImagen : "cuadrada"
     
     }
@@ -133,7 +77,7 @@ function numRandom(){
 
     
     
-    return  Math.floor( Math.random() * 10 )    
+    return  Math.floor( Math.random() * datos.length )    
     
     
 }
@@ -142,46 +86,46 @@ function numRandom(){
 
 function renderTexto(){ //apertura funcion
 
-    
+    if(datos.length === 0){
+
+
+        document.getElementById("cuerpo").innerHTML = ""
+        
+        document.getElementById("mostrar").innerHTML = `
+
+        <h3 class="texto" > Bueno, eso es todo. Tampoco soy tan interesante.</h3>
+
+
+        `
+
+    }else{
     let num = numRandom() //para que las funciones tengan el mismo numero random
 
 
-     if(datos[num].tipoImagen === "cuadrada"){
-  
-    document.getElementById("mostrar").innerHTML = `
-
-    <h3 class="texto" >${datos[num].texto}</h3>
     
-    <img class="cuadradra" src="${datos[num].imagen}" />
-    
-    `
-    }else if (datos[num].tipoImagen === "vertical"){
-
   
         document.getElementById("mostrar").innerHTML = `
-    
-        <h3 class="texto" >${datos[num].texto}</h3>
-        
-        <img class="vertical" src="${datos[num].imagen}" />
-        
-        `
 
+         <h3 class="texto" >${datos[num].texto}</h3>
     
-    }else{
-    document.getElementById("mostrar").innerHTML = `
-    
-    <h3 class="texto" >${datos[num].texto}</h3>
-    
-    <img class="horizontal" src="${datos[num].imagen}" />
+         <img class=${datos[num].tipoImagen} src="${datos[num].imagen}" />
     
     `
+
+    datos.splice(num,1);
+  // datos[num].repeticiones += 1
+
+}
+
+
+
   
-    }
+    }//cierre de funcion
        
     
         
 
-} //cierre de funcion
+ 
 
 
 
@@ -190,12 +134,14 @@ randomBtn.addEventListener("click", function() {
 
     document.getElementById("mostrar").innerHTML="";
     
-    
+
     renderTexto()
-   
+ 
+
     //datos[num].repeticiones += 1;
    
 })
 
-   
+
+
 
