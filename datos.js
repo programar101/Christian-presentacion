0b09f6c0641852e223ab8adf631d80c188cde5b7
Christian Carballo <carballo.chr@gmail.com>
2021-12-07 00:20:02 -0300

    Clase 2/!2

---
+++ datos.js
@@ -0,0 +1,53 @@
+let datos = [
+    {
+        texto: 'Tengo 25 años y cumplo el 04 de Octubre.',
+        imagen: new Image().src = 'https://sites.google.com/site/informaticaprueba1emmanuel/_/rsrc/1395877037694/dohko-de-libra/hqdefault.jpg',
+        tipoImagen: "horizontal"
+    },
+    {
+        texto: 'Soy de Costa Rica',
+        imagen: new Image().src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Mapa_CR.svg/1200px-Mapa_CR.svg.png",
+        tipoImagen: "cuadrada"
+    },
+    {
+        texto: 'Vivo en Florida Oeste.',
+        imagen: new Image().src = "https://upload.wikimedia.org/wikipedia/commons/c/cb/Estaci%C3%B3n_Florida_de_la_l%C3%ADnea_Belgrano_Norte_en_Florida_Oeste.jpg",
+        tipoImagen: "horizontal"
+    },
+    {
+        texto: ' Mi libro favorito es "Hombres de Maíz" de Miguel Angel de Asturias',
+        imagen: new Image().src = "https://contentv2.tap-commerce.com/cover/large/9789500396332_1.jpg?id_com=1113",
+        tipoImagen: "vertical"
+    },
+    {
+        texto: 'Actualmente estoy leyendo la Saga de "Canción de Hielo y Fuego" de George R.R. Martin.',
+        imagen: new Image().src = "https://www.penguinlibros.com/uy/239669-large_default/danza-de-dragones-5.jpg",
+        tipoImagen: "vertical"
+    },
+    {
+        texto: 'Sé tocar un poco la guitarra y tengo una banda llamada "Sidra"',
+        imagen: new Image().src = "https://www.educaciontrespuntocero.com/wp-content/uploads/2017/11/peliculas_prehistoria_destacada.jpg",
+        tipoImagen: "horizontal"
+    },
+    {
+        texto: 'Soy horrible jugando al ajedrez pero juego seguido.',
+        imagen: new Image().src = "https://upload.wikimedia.org/wikipedia/commons/a/ab/Mate_pastor_ajedrez.gif",
+        tipoImagen: "cuadrada"
+    },
+    {
+        texto: 'Mi película favorita es "El Padrino"',
+        imagen: new Image().src = "https://static.wikia.nocookie.net/doblaje/images/9/9a/Elpadrino.jpg/revision/latest?cb=20170311061852&path-prefix=es",
+        tipoImagen: "vertical"
+    },
+    {
+        texto: 'Mi segunda película favorita es "American History X"',
+        imagen: new Image().src = "https://pics.filmaffinity.com/American_History_X-201185607-large.jpg",
+        tipoImagen: "vertical"
+    },
+    {
+        texto: 'Mi banda favoita es: ',
+        imagen: new Image().src = "https://i0.wp.com/www.scienceofnoise.net/wp-content/uploads/2020/09/rust-in-peace-5cd5b40e937e4.jpg",
+        tipoImagen: "cuadrada"
+
+    }
+];
--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 
 function numRandom() {
 
-
-
     return Math.floor(Math.random() * datos.length)
-
 
 }
 
@@ -33,14 +30,15 @@ function renderTexto() { //apertura funcion
 
     } else {
 
-        let num = numRandom() //para que las funciones tengan el mismo numero random
+        const num = numRandom() //para que las funciones tengan el mismo numero random
+        const dato =datos[num]
 
 
         document.getElementById("mostrar").innerHTML = `
 
-         <h3 class="texto" >${datos[num].texto}</h3>
+         <h3 class="texto" >${dato.texto}</h3>
     
-         <img class=${datos[num].tipoImagen} src="${datos[num].imagen}" />
+         <img class=${dato.tipoImagen} src="${dato.imagen}" />
     
     `
         /*
@@ -65,82 +63,20 @@ function renderTexto() { //apertura funcion
 }//cierre de funcion
 
 
-function reinicio_func() {
+// function reinicio_func() {
    
-    location.reload()
+//     location.reload()
 
-/*
-    document.getElementById("cuerpo").innerHTML = pagInicio;
-    numRandom()
+// /*
+//     document.getElementById("cuerpo").innerHTML = pagInicio;
+//     numRandom()
 
-}
+// }
 
-*/
-}
+// */
+// }
 
-let datos = [
-    {
-        texto: 'Tengo 25 años y cumplo el 04 de Octubre.',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = 'https://sites.google.com/site/informaticaprueba1emmanuel/_/rsrc/1395877037694/dohko-de-libra/hqdefault.jpg',
-        tipoImagen: "horizontal"
-    },
-    {
-        texto: 'Soy de Costa Rica',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Mapa_CR.svg/1200px-Mapa_CR.svg.png",
-        tipoImagen: "cuadrada"
-    },
-    {
-        texto: 'Vivo en Florida Oeste.',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = "https://upload.wikimedia.org/wikipedia/commons/c/cb/Estaci%C3%B3n_Florida_de_la_l%C3%ADnea_Belgrano_Norte_en_Florida_Oeste.jpg",
-        tipoImagen: "horizontal"
-    },
-    {
-        texto: ' Mi libro favorito es "Hombres de Maíz" de Miguel Angel de Asturias',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = "https://contentv2.tap-commerce.com/cover/large/9789500396332_1.jpg?id_com=1113",
-        tipoImagen: "vertical"
-    },
-    {
-        texto: 'Actualmente estoy leyendo la Saga de "Canción de Hielo y Fuego" de George R.R. Martin.',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = "https://www.penguinlibros.com/uy/239669-large_default/danza-de-dragones-5.jpg",
-        tipoImagen: "vertical"
-    },
-    {
-        texto: 'Sé tocar un poco la guitarra y tengo una banda llamada "Sidra"',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = "https://www.educaciontrespuntocero.com/wp-content/uploads/2017/11/peliculas_prehistoria_destacada.jpg",
-        tipoImagen: "horizontal"
-    },
-    {
-        texto: 'Soy horrible jugando al ajedrez pero juego seguido.',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = "https://upload.wikimedia.org/wikipedia/commons/a/ab/Mate_pastor_ajedrez.gif",
-        tipoImagen: "cuadrada"
-    },
-    {
-        texto: 'Mi película favorita es "El Padrino"',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = "https://static.wikia.nocookie.net/doblaje/images/9/9a/Elpadrino.jpg/revision/latest?cb=20170311061852&path-prefix=es",
-        tipoImagen: "vertical"
-    },
-    {
-        texto: 'Mi segunda película favorita es "American History X"',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = "https://pics.filmaffinity.com/American_History_X-201185607-large.jpg",
-        tipoImagen: "vertical"
-    },
-    {
-        texto: 'Mi banda favoita es: ',
-        repeticiones: 0, //variable vacía para almacenar el numero de intentos
-        imagen: new Image().src = "https://i0.wp.com/www.scienceofnoise.net/wp-content/uploads/2020/09/rust-in-peace-5cd5b40e937e4.jpg",
-        tipoImagen: "cuadrada"
 
-    }
-];
 
 let guardaArchivos = [] //acá se guardan los objetos que borro de "datos"
 
@@ -157,8 +93,8 @@ let pagFinal = document.getElementById("cuerpo").innerHTML = `
     </div>      
      
         <img class="imgFinal" src="https://c.tenor.com/U2JqGqBXqZgAAAAM/cat-sad.gif" />
-        
-        <button onclick="reinicio_func()">Reiniciar</button>
+<button  id="random-btn">Dato Random</button>
+        <button id="reinicio">Reiniciar</button>
        `
 
 
@@ -184,6 +120,11 @@ let textoVacio = ""
 const mostrar = document.getElementById("mostrar")
 
 const randomBtn = document.getElementById("random-btn")
+
+
+
+
+
 
 randomBtn.addEventListener("click", function () {
 
